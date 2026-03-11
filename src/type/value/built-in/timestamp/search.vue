1ed14a749ccec423eb307f2765c5bf2fb6617d6f
icecream <www@withdata.net>
2026-03-11 18:18:54 +0800

    fix: 为时间选择器添加 value-format 属性以支持时间戳格式

--- src/type/value/built-in/timestamp/search.vue
+++ src/type/value/built-in/timestamp/search.vue
@@ -3,9 +3,10 @@
         v-if="options.concat('YYYY-MM-DD')"
         :type="options.concat('HH:mm:ss') ? 'datetime' : 'date'"
         v-model="model"
+        value-format="x"
         clearable
     />
-    <el-time-picker v-else v-model="model" clearable/>
+    <el-time-picker v-else v-model="model" value-format="x" clearable/>
 </template>
 
 <script setup lang="ts">
