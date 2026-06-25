2cf4ec37aa7de09f630766aabd71ddb47b8067b8
icecream <www@withdata.net>
2026-06-26 00:48:37 +0800

    fix: 修复安装时仅在未设置选项或提供新选项时更新 options 属性

--- src/ako.ts
+++ src/ako.ts
@@ -113,7 +113,7 @@ export class Ako implements ObjectPlugin<AkoOptions> {
 
     install(app: App, options?: AkoOptions) {
         this.app = app
-        if (options) this.options = options
+        if (!this.options || options) this.options = options
 
         app.provide(AkoSymbol, this)
         app.provide(AkoOptionsSymbol, this._options)
